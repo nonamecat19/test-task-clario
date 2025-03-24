5450306bf337af212ccd5cf41bcb6a7f3d135542
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-03-24 17:34:59 +0200

    feat: add body validation for requests

---
+++ lib/validate.ts
@@ -0,0 +1,23 @@
+import {ZodSchema} from "zod";
+
+type Success<T> = { success: true; data: T }
+type Failure = { success: false; data: null }
+
+export async function validateBody<T>(
+    req: Request,
+    schema: ZodSchema<T>
+): Promise<Success<T> | Failure> {
+    try {
+        const json = await req.json()
+        const parsed = schema.parse(json)
+        return {
+            success: true,
+            data: parsed
+        }
+    } catch {
+        return {
+            success: false,
+            data: null
+        };
+    }
+}
